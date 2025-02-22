e87f5c721ec79e0c62086e2711308d0569f7f0c6
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-02-22 13:28:46 +0700

    set host for prefect server

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Start Prefect server if COMPONENT is "server"
 if [ "$COMPONENT" = "server" ]; then
     prefect config set PREFECT_API_URL='https://weasley-production.up.railway.app/api'
-    prefect server start --port $PORT
+    prefect server start --port $PORT --host 0.0.0.0
 
 # Start Prefect worker if COMPONENT is "worker"
 elif [ "$COMPONENT" = "worker" ]; then
